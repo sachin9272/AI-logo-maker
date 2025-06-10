f76adc5e83ae87527aae4fe9d391fbb3f54f2844
Sachin Singh <sachinsingh9272@gmail.com>
2025-06-10 18:49:23 +0530

    Implemented Hero Section

---
+++ app/_data/Lookup.jsx
@@ -0,0 +1,6 @@
+export default{
+    HeroHeading:'AI Logo Maker',
+    HeroSubHeading:'Perfect Logos for Apps, Businesses, and Websites',
+    HeroDesc: 'Craft unique and professional logos effortlessly with our AI-powered tool. Perfect for apps, businesses, websites and more!',
+    InputTitlePlaceholder:'Enter your Logo Name'
+}
--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Host_Grotesk, Host_Grotesk } from "next/font/google";
 import "./globals.css";
+import Provider from "./provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const host_Grotesk = Host_Grotesk({
+  subsets:['latin']
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -20,9 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={host_Grotesk.className}
       >
-        {children}
+        <Provider>
+          {children}
+        </Provider>
       </body>
     </html>
   );
