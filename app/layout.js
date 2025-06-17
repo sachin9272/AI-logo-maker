d495e580517ff6acd5a62074411b6c76551708bc
Sachin Singh <sachinsingh9272@gmail.com>
2025-06-17 20:02:30 +0530

    Implemented Clerk with protected route

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Host_Grotesk, Host_Grotesk } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
-
+import { ClerkProvider } from "@clerk/nextjs";
 const host_Grotesk = Host_Grotesk({
   subsets:['latin']
 })
@@ -13,6 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={host_Grotesk.className}
@@ -22,5 +23,6 @@ export default function RootLayout({ children }) {
         </Provider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
