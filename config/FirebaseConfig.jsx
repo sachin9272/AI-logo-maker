d996c4ae23853d52f4990ccea16610c0b62daf7b
Sachin Singh <sachinsingh9272@gmail.com>
2025-06-22 23:11:17 +0530

    Implemented Firebase config

---
+++ config/FirebaseConfig.jsx
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBpOirLVo6sWXjN1yr-bXovBKg0LmL_PW4",
+  authDomain: "ai-logo-maker-87a2c.firebaseapp.com",
+  projectId: "ai-logo-maker-87a2c",
+  storageBucket: "ai-logo-maker-87a2c.firebasestorage.app",
+  messagingSenderId: "160256365820",
+  appId: "1:160256365820:web:a0ce2e3021f7984d87f379",
+  measurementId: "G-QFF65MRVR8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
